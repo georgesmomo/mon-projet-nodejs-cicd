b70e10e173740a4b4206358655b076504c5c0f36
G. MOMO <6159388+georgesmomo@users.noreply.github.com>
2025-07-14 15:54:52 +0200

    mm

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 
 const routes = require('./routes/route.js');
 
+const client = require('prom-client');
+const register = new client.Registry();
+
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
@@ -10,6 +13,12 @@ app.get('/', routes.home);
 
 app.get('/healthz', (req, res) => {
   res.status(200).send('OK');
+});
+
+// Expose le point de terminaison /metrics
+app.get('/metrics', async (req, res) => {
+  res.setHeader('Content-Type', register.contentType);
+  res.end(await register.metrics());
 });
 
 const port = process.env.PORT || 3000;
